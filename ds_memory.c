eb5c6de14ee05aea86b6e57fa0dbf52fa0e31950
Lilian Shi <lshi02@uoguelph.ca>
2019-09-10 21:00:52 -0400

    added ds_memory files (very incomplete!!!!!!!!!!)

---
+++ ds_memory.c
@@ -0,0 +1,19 @@
+/*
+Name: Lilian Shi
+Stu#: 1048355
+Email: lshi02@uoguelph.ca
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ds_file_struct ds_file;
+struct ds_counts_struct ds_counts;
+
+/*
+filename: name of file to create/write
+size: 
+*/
+int ds_create(char *filename, long size){
+
+}
