e67ea43482e20a3e7ab0b1929858f370003727c7
Lilian Shi <lshi02@uoguelph.ca>
2019-09-14 20:49:11 -0400

    made main.c

---
+++ a1/ds_memory.c
@@ -0,0 +1,24 @@
+/*
+Name: Lilian Shi
+Stu#: 1048355
+Email: lshi02@uoguelph.ca
+*/
+
+#include "ds_memory.h"
+
+struct ds_file_struct ds_file;
+struct ds_counts_struct ds_counts;
+
+/*
+filename: name of file to create/write
+size: length of first array entry
+ds_create: create file of filename; write "header" (ds_file.block) into file
+*/
+int ds_create(char *filename, long size){
+  printf("sma");
+
+/*  ds_file->fp = fopen(filename, "wb");
+  console.log(sizeof(struct ds_block));
+*/
+  //ds_file.fp = fwrite(ds_file.block, sizeof(struct ds_block))
+}
